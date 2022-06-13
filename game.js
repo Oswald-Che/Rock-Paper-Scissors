cdfb7c402bd00b71f16885581f623b2b432d0b08
Oswald-Che <cheoswald@yahoo.com>
2022-06-13 18:04:49 +0100

    Update Full game function

--- game.js
+++ game.js
@@ -19,19 +19,47 @@ function playRound(playerSelection , computerSelection ){
 }
 let playerCount = 0
 let computerCount = 0
-
-for(let i = 0; i < 5; i++){
-const playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase()
+let count
+let draw = 0
+function gameInfo(a){
+    if(a==0) return prompt("Round 1\n Enter Rock, Paper or Scissors").toLowerCase();
+     else if(a==1) return prompt("Round 2 \nEnter Rock, Paper or Scissors").toLowerCase();   
+    else if(a==2) return prompt("Round 3 \nEnter Rock, Paper or Scissors").toLowerCase();
+    else if(a==3) return prompt("Round 4\nEnter Rock, Paper or Scissors").toLowerCase();
+    else return  prompt("Round 5\nEnter Rock, Paper or Scissors").toLowerCase();
+    }
+function game(n){
+for(let i = 0; i < n && n<6; i++){
+const playerSelection = gameInfo(i)
 const computerSelection = computerPlay()
 console.log("computer plays " + computerSelection)
 console.log("You play " + playerSelection)
 console.log(playRound(playerSelection , computerSelection) )
-let count = playRound(playerSelection , computerSelection).slice(0 , 7)
+count = playRound(playerSelection , computerSelection).slice(0 , 7)
+alert(playRound(playerSelection , computerSelection))
 if ( count == "You win") playerCount +=  1
-else if ( count == "Draw") playerCount += 0
+else if ( count == "Draw") draw += 1
 else computerCount = computerCount + 1
 }
-console.log(computerCount)
-if(playerCount > computerCount) console.log("Congratulations You have Triumphed over computer")
-else if (playerCount < computerCount) console.log("You lost over computer")
-else console.log("You drew")
+if(playerCount > computerCount) {
+    alert(`Congratulations!!! You won!! \n Player score-${playerCount}/${n} \
+    Computer score-${computerCount}/${n} Draws-${draw}/${n}` )
+    console.log(`Congratulations!!! You won!\n Player score-${playerCount}/${n} \
+    Computer score-${computerCount}/${n} Draws-${draw}/${n}`)
+}
+else if (count == undefined){
+     alert("Input 5 or less")
+    console.log("Input 5 or less")}
+else if (playerCount < computerCount){
+     alert(`You lost! Better luck next \n Player score-${playerCount}/${n} \
+     Computer score-${computerCount}/${n} Draws-${draw}/${n}`) 
+    console.log(`You lost! Better luck next \n Player score-${playerCount}/${n} \
+    Computer score-${computerCount}/${n} Draws-${draw}/${n}`)}
+else {
+    alert(`You drew \n Player score-${playerCount}/${n} \
+    Computer score-${computerCount}/${n} Draws-${draw}/${n}`)
+    console.log(`You drew \n Player score-${playerCount}/${n} \
+    Computer score-${computerCount}/${n} Draws-${draw}/${n}`)}
+}
+let x = parseInt(prompt("Input number of rounds \n Maximum 5"))
+game(x)
