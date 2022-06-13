c6038f23bd4a7f0450d35dc24f9914441e9e6a36
Oswald-Che <cheoswald@yahoo.com>
2022-06-13 14:20:05 +0100

    Add Function for one round of play and Fix bugs

--- game.js
+++ game.js
@@ -1,7 +1,25 @@
+// function for computer choices
 function computerPlay(){
     let a = Math.random()
-    if(a < 1/3) return "Rock" 
-    else if (1/3 > 0.33 && a < 2/3) return "Paper" 
-    else return "Scissors" 
+     if(a < 1/3) return "rock" 
+    else if (1/3 < a && a < 2/3) return "paper" 
+    else return "scissors" 
 }
-console.log(computerPlay())
+
+function playRound(playerSelection , computerSelection ){
+    if(playerSelection === "rock" && computerSelection === "scissors") return "You win! Rock beats Scissors" ;
+    else if (playerSelection === "paper" && computerSelection === "rock") return "You win Paper cover Rock";
+    else if(playerSelection === "scissors" && computerSelection === "paper") return "You win Scissors cut Paper";
+    else if (playerSelection === computerSelection) return "Draw";
+    else if (playerSelection === "scissors" && computerSelection === "rock") return "You Lose! Can't beat Rock";
+    else if(playerSelection === "rock" && computerSelection === "paper") return "You Lose! Paper destroys Rock";
+    else if (playerSelection === "paper" && computerSelection === "scissors") return "You Lose! Scissors slices Paper";
+    else return "Wrong input choices are Rock, Paper or Scissors";
+   
+}
+const playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase()
+//let playerSelection = "rock"
+const computerSelection = computerPlay()
+console.log("computer plays " + computerSelection)
+console.log("You play " + playerSelection)
+console.log(playRound(playerSelection , computerSelection) )
